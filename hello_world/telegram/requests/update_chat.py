e805390c8ec4a91db318ccad2f107819f1b40d99
eduardobrl <eduardobrl10@gmail.com>
2024-02-10 19:46:33 -0300

    adiciona campos opcionais para quando mensagem vem em groupchat

--- hello_world/telegram/requests/update_chat.py
+++ hello_world/telegram/requests/update_chat.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from pydantic import BaseModel, Field
 
 class From(BaseModel):
@@ -10,9 +11,9 @@ class From(BaseModel):
 
 class Chat(BaseModel):
   id: int
-  first_name: str
-  last_name: str
-  username: str
+  first_name: Optional[str] = None
+  last_name: Optional[str] = None
+  username: Optional[str] = None
   type: str
 
 class Message(BaseModel):
@@ -20,7 +21,7 @@ class Message(BaseModel):
   from_: From = Field(alias='from')
   chat: Chat 
   date: int
-  text: str
+  text: Optional[str] = None
 
 class Update(BaseModel):
   update_id: int
