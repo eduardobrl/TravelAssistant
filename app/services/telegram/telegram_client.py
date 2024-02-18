fffe86d56c9d789849d8fed1089eb2fbff1306e9
eduardobrl <eduardobrl10@gmail.com>
2024-02-17 22:57:23 -0300

    ajuste telegram client

--- app/services/telegram/telegram_client.py
+++ app/services/telegram/telegram_client.py
@@ -65,5 +65,5 @@ class TelegramClient:
     
     async def download_file(self, file: File):  
         async with aiohttp.ClientSession() as session:
-            async with session.get(self.FILE_URL + "/" + file.file_path) as response:
+            async with session.get(self.FILE_URL + "/" + file["result"]["file_path"]) as response:
                 return await response.content
