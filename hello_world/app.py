a0a3383ec18b0a888884735ba9e03089224cca37
eduardobrl <eduardobrl10@gmail.com>
2024-02-17 21:08:53 -0300

    add lambda to index file docuemnts

--- hello_world/app.py
+++ hello_world/app.py
@@ -1,13 +1,10 @@
 import asyncio
 import json
-
-import boto3
+import logging
 from domain.constants import MessagesConstants
 from services.openai.openai_client import OpenAiClient
 from services.repositories.chat_repository import ChatRepository, ChatRole
-import logging
 from services.telegram.requests.update_chat import Update
-
 from services.telegram.telegram_client import TelegramClient
 
 
@@ -45,27 +42,6 @@ async def async_stream_handler(event, context):
         
 
 async def async_lambda_handler(event, context):
-    """Sample pure Lambda function
-
-    Parameters
-    ----------
-    event: dict, required
-        API Gateway Lambda Proxy Input Format
-
-        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
-
-    context: object, required
-        Lambda Context runtime methods and attributes
-
-        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
-
-    Returns
-    ------
-    API Gateway Lambda Proxy Output Format: dict
-
-        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
-    """
-    
     telegram = TelegramClient()
     openai = OpenAiClient()
     repository = ChatRepository()
